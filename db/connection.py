8eb25ffab9cdbeee271accdf9d9329abd1bfc9b3
johnatrootdynamics <121533148+johnatrootdynamics@users.noreply.github.com>
2025-11-28 09:22:33 -0500

    123

--- db/connection.py
+++ db/connection.py
@@ -1,7 +1,7 @@
 import os
 import mysql.connector
 
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_HOST = os.getenv("DB_HOST", "srv-captain--carbookdb-db")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASS = os.getenv("DB_PASS", "")
